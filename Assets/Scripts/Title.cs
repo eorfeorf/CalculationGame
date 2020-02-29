5a865662cd6e71b44dd0ff5a6da34de27dfcc1f0
eorfeorf <orerrurirar2@gmail.com>
2020-03-01 01:07:19 +0900

    Spaceキーでゲームシーンに遷移できるように実装。

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    private ToScene toScene;
+    void Start()
+    {
+        toScene = GetComponent<ToScene>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            toScene.Translate();
+        }
+    }
+}
